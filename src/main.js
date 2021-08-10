dd20073bd5586383809ae6d7bdbc768309f2969c
Yu Fang Chin <>
2021-08-10 14:38:18 +0800

    currency exchange function

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { BootstrapVue } from "bootstrap-vue";
+import AsyncComputed from "vue-async-computed";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(AsyncComputed);
 
 new Vue({
 	render: (h) => h(App),
